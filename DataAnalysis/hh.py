134b74a7fd0558b66861b8218f14b87ae4ec04c8
Muhammadqodir <m.qodir777@gmail.com>
2022-11-27 01:23:43 +0300

    Configures App for iOS & improved DataAnalysis system

--- DataAnalysis/hh.py
+++ DataAnalysis/hh.py
@@ -1,20 +1,35 @@
 import requests
+import json
 
 job = input("Job: ")
-response = requests.get("https://api.hh.ru/vacancies?text="+job+"&per_page=40")
+response = requests.get("https://api.hh.ru/vacancies?text="+job+"&per_page=40&only_with_salary=true&search_field=name")
 
 data = response.json()
 print("Items count: "+str(data["found"]))
 
-skills = []
+skills = {}
 for item in data["items"]:
   if item["salary"] != None:
-    print(item["name"]+"("+str(item["salary"]["from"])+"-"+str(item["salary"]["to"])+")")
-    key_skills = requests.get(item["url"]).json()["key_skills"]
-    for key in key_skills:
-      skills.append(key["name"])
+    salary = str(item["salary"]["from"])+"-"+str(item["salary"]["to"])
+    salary = item["salary"]["from"]
+    print(item["name"]+"("+salary+")")
+    key_skills = requests.get(item["url"]).json()
+    for key in key_skills["key_skills"]:
+      if key["name"] in skills:
+        skills[key["name"]]["frequency"] += 1
+        skills[key["name"]]["items"].append({"title": key_skills["name"], "salary":salary})
+      else:
+        init_data = {
+          "frequency": 1,
+          "items": [
+            {
+              "title": key_skills["name"],
+              "salary": salary
+            }
+          ]
+        }
+        skills[key["name"]] = init_data
     print("-------------------------\n")
 
 print("\n\nSkills:")
-my_dict = {i:skills.count(i) for i in skills}
-print(my_dict)
+print(json.dumps(skills, indent = 4))
